bd63e564474f2c15c9e194b2df40e401d263b43c
Thunderbird93 <antircristian@gmail.com>
2022-12-12 16:00:34 +0100

    [add] Toy module

--- Models/Toy.php
+++ Models/Toy.php
@@ -1,3 +1,14 @@
 <?php
 
+class Toy extends Product{
+    public $feature;
+    public $size;
+
+    public function __construct($_feature, $_size)
+    {
+        $this->feature = $_feature;
+        $this->size = $_size;
+    }
+}
+
 ?>
