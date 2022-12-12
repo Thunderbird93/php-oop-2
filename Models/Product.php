7d6b3c4d273a36c1718f19ff12504b03517b495d
Thunderbird93 <antircristian@gmail.com>
2022-12-12 15:32:48 +0100

    [add] construct

--- Models/Product.php
+++ Models/Product.php
@@ -8,5 +8,16 @@ class Product{
     public $price;
     public $is_available;
     public $image;
+
+    public function __construct($id, $name, $brand, $category, $price, $is_available, $image)
+    {
+        $this -> id = $_id;
+        $this -> name = $_name;
+        $this -> brand = $_brand;
+        $this -> category = $_category;
+        $this -> price = $_price;
+        $this -> is_available = $_is_available;
+        $this -> image = $_image;
+    }
 }
 ?>
