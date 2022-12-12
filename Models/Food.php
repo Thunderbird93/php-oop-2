b2a7f9adf817b069428be4d14ca24aff46b5352c
Thunderbird93 <antircristian@gmail.com>
2022-12-12 16:09:26 +0100

    [add] Food module

--- Models/Food.php
+++ Models/Food.php
@@ -1,3 +1,16 @@
 <?php
 
+class Food extends Product{
+    public $weight;
+    public $ingredients;
+    public $taste;
+
+    public function __construct($_weight, $_ingredients, $_taste)
+    {
+        $this->weight = $_weight;
+        $this->ingredients = $_ingredients;
+        $this->taste = $_taste;
+    }
+}
+
 ?>
