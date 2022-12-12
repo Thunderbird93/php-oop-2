895286791299903dbd4b69b72bb6a4ef2854a605
Thunderbird93 <antircristian@gmail.com>
2022-12-12 15:10:34 +0100

    [add] Modules to Index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
+require_once __DIR__ . '/Models/Accessory.php';
+require_once __DIR__ . '/Models/Category.php';
+require_once __DIR__ . '/Models/Food.php';
+require_once __DIR__ . '/Models/Product.php';
+require_once __DIR__ . '/Models/Toy.php';
+
 
 ?>
