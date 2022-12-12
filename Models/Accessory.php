84f260d6870c693febd571fbc79cc75093c12c7e
Thunderbird93 <antircristian@gmail.com>
2022-12-12 16:05:55 +0100

    [add] accessory module

--- Models/Accessory.php
+++ Models/Accessory.php
@@ -1,3 +1,14 @@
 <?php
 
+class Accessory extends Product{
+    public $material;
+    public $size;
+
+    public function __construct($_material, $_size)
+    {
+        $this->material = $_material;
+        $this->size = $_size;
+    }
+}
+
 ?>
